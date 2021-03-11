5d499877ffa4db50d70cc62ddd38e956cd8b4e9e
feed9001 <eduardo.ferrandez@nielseniq.com>
2021-03-11 12:53:04 +0100

    add threshold to request

--- counter/domain/actions.py
+++ counter/domain/actions.py
@@ -3,7 +3,7 @@ from typing import List
 from PIL import Image
 
 from counter.domain.ports import ObjectDetector, ObjectCountRepo
-from counter.domain.models import Prediction, CountResponse
+from counter.domain.models import CountResponse
 from counter.debug import draw
 from counter.domain.predictions import over_threshold, count
 
@@ -13,11 +13,12 @@ class CountDetectedObjects:
         self.__object_detector = object_detector
         self.__object_count_repo = object_count_repo
 
-    def execute(self, image, threshold) -> List[Prediction]:
+    def execute(self, image, threshold) -> CountResponse:
         predictions = self.__find_valid_predictions(image, threshold)
         object_counts = count(predictions)
         self.__object_count_repo.update_values(object_counts)
-        return CountResponse(current_objects=object_counts, total_objects=self.__object_count_repo.read_values())
+        total_objects = self.__object_count_repo.read_values()
+        return CountResponse(current_objects=object_counts, total_objects=total_objects)
 
     @staticmethod
     def __debug_image(image, predictions, image_name):
@@ -28,6 +29,6 @@ class CountDetectedObjects:
     def __find_valid_predictions(self, image, threshold):
         predictions = self.__object_detector.predict(image)
         self.__debug_image(image, predictions, "all_predictions.jpg")
-        predictions = list(over_threshold(predictions, threshold=threshold))
-        self.__debug_image(image, predictions, f"valid_predictions_with_threshold_{threshold}.jpg")
-        return predictions
+        valid_predictions = list(over_threshold(predictions, threshold=threshold))
+        self.__debug_image(image, valid_predictions, f"valid_predictions_with_threshold_{threshold}.jpg")
+        return valid_predictions
--- counter/entrypoints/webapp.py
+++ counter/entrypoints/webapp.py
@@ -12,9 +12,10 @@ count_action = config.get_count_action()
 @app.route('/object-count', methods=['POST'])
 def object_detection():
     uploaded_file = request.files['file']
+    threshold = float(request.form.get('threshold', 0.5))
     image = BytesIO()
     uploaded_file.save(image)
-    count_response = count_action.execute(image, 0.5)
+    count_response = count_action.execute(image, threshold)
     return jsonify(count_response)
 
 
@@ -22,8 +23,7 @@ if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
 
 """
-curl \
+curl -F "threshold=0.9" \
   -F "file=@/home/feed9001/work/spikes/object_detection_tfs/coco/val/val2017/000000010363.jpg" \
   http://0.0.0.0:5000/object-count
-  
 """
